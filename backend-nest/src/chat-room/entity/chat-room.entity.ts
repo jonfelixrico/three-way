f80e49a297304de394c63af44e1f7e0ad70e5d4c
Felix <jonfelixrico@gmail.com>
2024-05-01 19:39:16 +0800

    Add TODO

--- backend-nest/src/chat-room/entity/chat-room.entity.ts
+++ backend-nest/src/chat-room/entity/chat-room.entity.ts
@@ -3,6 +3,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm'
 
 @Entity()
 export class ChatRoom implements IChatRoom {
+  // TODO change this to PrimaryGeneratedColumn once we're not setting the id manually for some cases
   @PrimaryColumn({
     type: 'varchar',
     generated: 'uuid',
