c24d60f881f727842ce34fcd810bdea1c951d122
Felix <jonfelixrico@gmail.com>
2024-05-01 17:28:23 +0800

    Index some columns

--- backend-nest/src/chat-room/entity/chat-room-message.entity.ts
+++ backend-nest/src/chat-room/entity/chat-room-message.entity.ts
@@ -1,6 +1,6 @@
 import { ChatRoom } from './chat-room.entity'
 import { IChatRoomMessage } from './chat-room-entity.types'
-import { Column, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 
 export class ChatRoomMessage implements IChatRoomMessage {
   @PrimaryGeneratedColumn('uuid')
@@ -16,6 +16,7 @@ export class ChatRoomMessage implements IChatRoomMessage {
   @Column('text')
   content: string
 
+  @Index() // chat messages are typically presented as sorted by send timestamps, so we have to index this to keep queries fast
   @Column('time with time zone')
   timestamp: Date
 }
