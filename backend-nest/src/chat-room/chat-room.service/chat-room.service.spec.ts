e3ff0186b45fb8141c3e46c0d7c7cc372178ed6e
Felix <jonfelixrico@gmail.com>
2024-05-01 17:54:59 +0800

    Satisfy the tests

--- backend-nest/src/chat-room/chat-room.service/chat-room.service.spec.ts
+++ backend-nest/src/chat-room/chat-room.service/chat-room.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ChatRoomService } from './chat-room.service'
+import {
+  CHAT_ROOM_MESSAGE_REPOSITORY_PROVIDER,
+  CHAT_ROOM_REPOSITORY_PROVIDER,
+} from 'src/chat-room/chat-room.constants'
+import { createMock } from '@golevelup/ts-jest'
+import { Repository } from 'typeorm'
+import { ChatRoomMessage } from 'src/chat-room/entity/chat-room-message.entity'
+import { ChatRoom } from 'src/chat-room/entity/chat-room.entity'
 
 describe('ChatRoomService', () => {
   let service: ChatRoomService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatRoomService],
+      providers: [
+        ChatRoomService,
+        {
+          provide: CHAT_ROOM_MESSAGE_REPOSITORY_PROVIDER,
+          useValue: createMock<Repository<ChatRoomMessage>>(),
+        },
+        {
+          provide: CHAT_ROOM_REPOSITORY_PROVIDER,
+          useValue: createMock<Repository<ChatRoom>>(),
+        },
+      ],
     }).compile()
 
     service = module.get<ChatRoomService>(ChatRoomService)
