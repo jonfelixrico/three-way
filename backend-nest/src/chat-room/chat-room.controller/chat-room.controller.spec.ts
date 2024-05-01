4d6cd3adc03cc5c4af3734d2c93c2945ec20a7d3
Felix <jonfelixrico@gmail.com>
2024-05-01 18:06:23 +0800

    Add functionalities

--- backend-nest/src/chat-room/chat-room.controller/chat-room.controller.spec.ts
+++ backend-nest/src/chat-room/chat-room.controller/chat-room.controller.spec.ts
@@ -1,5 +1,7 @@
+import { createMock } from '@golevelup/ts-jest'
 import { Test, TestingModule } from '@nestjs/testing'
 import { ChatRoomController } from './chat-room.controller'
+import { ChatRoomService } from 'src/chat-room/chat-room.service/chat-room.service'
 
 describe('ChatRoomController', () => {
   let controller: ChatRoomController
@@ -7,6 +9,12 @@ describe('ChatRoomController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatRoomController],
+      providers: [
+        {
+          provide: ChatRoomService,
+          useValue: createMock<ChatRoomService>(),
+        },
+      ],
     }).compile()
 
     controller = module.get<ChatRoomController>(ChatRoomController)
--- backend-nest/src/chat-room/chat-room.controller/chat-room.controller.ts
+++ backend-nest/src/chat-room/chat-room.controller/chat-room.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { ChatRoomService } from 'src/chat-room/chat-room.service/chat-room.service'
 
 @Controller('chat')
-export class ChatRoomController {}
+export class ChatRoomController {
+  constructor(private chatSvc: ChatRoomService) {}
+
+  @Get(':id')
+  async getChat(@Param('id') id: string) {
+    return await this.chatSvc.getChatRoomById(id)
+  }
+
+  @Get(':id/message')
+  async getMessages(@Param('id') id: string) {
+    return await this.chatSvc.getMessages({
+      chatId: id,
+    })
+  }
+
+  @Post(':id/message')
+  async sendMessage(@Param('id') id: string, @Body('message') message: string) {
+    return await this.chatSvc.sendMessage({
+      chatId: id,
+      message,
+    })
+  }
+}
