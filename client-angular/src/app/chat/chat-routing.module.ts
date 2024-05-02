a4c8bc2733f4e3219e5868b891991c56fb54eb84
Felix <jonfelixrico@gmail.com>
2024-05-03 00:28:35 +0800

    Integrate into the chat components

--- client-angular/src/app/chat/chat-routing.module.ts
+++ client-angular/src/app/chat/chat-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core'
+import { NgModule, inject } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { ChatComponent } from './chat.component'
+import { MessageService } from '../chat-rest-api/message.service'
+import { ChatRestApiModule } from '../chat-rest-api/chat-rest-api.module'
 
-const routes: Routes = [{ path: '', component: ChatComponent }]
+const routes: Routes = [
+  {
+    path: '',
+    component: ChatComponent,
+    resolve: {
+      messages: () => {
+        return inject(MessageService)!.getMessages('global')
+      },
+    },
+  },
+]
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), ChatRestApiModule],
   exports: [RouterModule],
 })
 export class ChatRoutingModule {}
--- client-angular/src/app/chat/chat.component.ts
+++ client-angular/src/app/chat/chat.component.ts
@@ -1,12 +1,28 @@
-import { Component, Input } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ChatMessage } from '../chat-rest-api/chat-rest-api.types'
+import { ActivatedRoute } from '@angular/router'
+import { firstValueFrom } from 'rxjs'
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
-export class ChatComponent {
-  @Input()
-  message: ChatMessage[] = []
+export class ChatComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  messages: ChatMessage[] = []
+
+  get serialized() {
+    return JSON.stringify(this.messages)
+  }
+
+  private async getDataFromResolver() {
+    const data = await firstValueFrom(this.activatedRoute.data)
+    this.messages = data['messages']
+  }
+
+  ngOnInit(): void {
+    this.getDataFromResolver()
+  }
 }
