a821917e86fab3c95d423d03f4546976696cf3ee
Felix <jonfelixrico@gmail.com>
2024-05-03 00:28:23 +0800

    Adjust exports

--- client-angular/src/app/chat-rest-api/chat-rest-api.module.ts
+++ client-angular/src/app/chat-rest-api/chat-rest-api.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { AxiosModule } from '../axios/axios.module'
+import { MessageService } from './message.service'
 
 @NgModule({
-  declarations: [],
+  providers: [MessageService],
   imports: [CommonModule, AxiosModule],
 })
 export class ChatRestApiModule {}
