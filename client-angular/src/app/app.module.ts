8933206bfd8a7754a133b808a80efb9dd86b84ce
Felix <jonfelixrico@gmail.com>
2024-05-02 21:05:50 +0800

    Restore SSR

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -7,7 +10,7 @@ import { AppComponent } from './app.component'
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
