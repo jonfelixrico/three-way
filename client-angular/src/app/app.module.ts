7249a444e2bae8c82d949a0ebb3d5874797617ca
Felix <jonfelixrico@gmail.com>
2024-05-03 20:29:33 +0800

    Use fetch

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -7,12 +7,12 @@ import {
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withFetch } from '@angular/common/http'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [provideClientHydration(), provideHttpClient()],
+  providers: [provideClientHydration(), provideHttpClient(withFetch())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
