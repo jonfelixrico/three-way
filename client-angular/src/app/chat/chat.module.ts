49c7a44008944209f4d4664fa68fcc5f2f3fad71
Felix <jonfelixrico@gmail.com>
2024-05-03 23:55:32 +0800

    Add input field

--- client-angular/src/app/chat/chat.module.ts
+++ client-angular/src/app/chat/chat.module.ts
@@ -4,9 +4,10 @@ import { CommonModule, NgFor } from '@angular/common'
 import { ChatRoutingModule } from './chat-routing.module'
 import { ChatComponent } from './chat.component'
 import { ChatMessageComponent } from '../chat-message/chat-message.component'
+import { InputTextModule } from 'primeng/inputtext'
 
 @NgModule({
   declarations: [ChatComponent, ChatMessageComponent],
-  imports: [CommonModule, ChatRoutingModule, NgFor],
+  imports: [CommonModule, ChatRoutingModule, NgFor, InputTextModule],
 })
 export class ChatModule {}
