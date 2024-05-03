c850decc691318cb00d3576116aedf091d3c83ea
Felix <jonfelixrico@gmail.com>
2024-05-04 00:36:41 +0800

    Add StoreModule to AppModule

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -9,10 +9,16 @@ import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { provideHttpClient, withFetch } from '@angular/common/http'
 import { provideLocalStorage } from './localstorage.provider'
+import { StoreModule } from './store/store.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule,
+  ],
   providers: [
     provideClientHydration(),
     provideHttpClient(withFetch()),
