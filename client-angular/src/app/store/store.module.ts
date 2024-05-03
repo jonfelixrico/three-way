17694390ca996a5bdac9f7cec0aff3cdb00691d7
Felix <jonfelixrico@gmail.com>
2024-05-04 01:31:34 +0800

    Integrate devtools

--- client-angular/src/app/store/store.module.ts
+++ client-angular/src/app/store/store.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { NgxsModule } from '@ngxs/store'
 import { ChatSlice } from '../chat-services/chat.slice'
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin'
 
 @NgModule({
-  imports: [CommonModule, NgxsModule.forRoot([ChatSlice])],
+  imports: [
+    CommonModule,
+    NgxsModule.forRoot([ChatSlice], {
+      // TODO set appropriately
+      developmentMode: true,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+  ],
 })
 export class StoreModule {}
