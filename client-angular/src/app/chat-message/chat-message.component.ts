8f8b265e641cb690c7c8aac14d3005ac6cd0438d
Felix <jonfelixrico@gmail.com>
2024-05-03 22:54:24 +0800

    Add content

--- client-angular/src/app/chat-message/chat-message.component.ts
+++ client-angular/src/app/chat-message/chat-message.component.ts
@@ -4,11 +4,13 @@ import { ChatMessage } from '../chat-rest-api/chat-rest-api.types'
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
-  styleUrl: './chat-message.component.scss',
 })
 export class ChatMessageComponent {
   @Input({
     required: true,
   })
   message!: ChatMessage
+
+  @Input()
+  isSender = false
 }
