39b58a59fd664c7a27d71900a40de918296ef633
Felix <jonfelixrico@gmail.com>
2024-05-05 00:48:28 +0800

    Adjust radius classes

--- client-angular/src/app/chat-message/chat-message.component.ts
+++ client-angular/src/app/chat-message/chat-message.component.ts
@@ -14,11 +14,43 @@ export class ChatMessageComponent {
   @Input()
   isSender = false
 
+  @Input()
+  isFirstInSequence = false
+
+  @Input()
+  isLastInSequence = false
+
   get senderClasses() {
     if (!this.isSender) {
       return ['surface-200', 'align-items-start']
     }
 
     return ['align-items-end', 'bg-primary', 'text-white']
+  }
+
+  get radiusClasses() {
+    const classes: string[] = []
+
+    if (this.isSender) {
+      classes.push('border-round-left-3xl', 'border-round-right-lg')
+    } else {
+      classes.push('border-round-right-3xl', 'border-round-left-lg')
+    }
+
+    if (this.isFirstInSequence && !this.isLastInSequence) {
+      classes.push('border-round-bottom-lg', 'border-round-top-3xl')
+    } else if (!this.isFirstInSequence && this.isLastInSequence) {
+      classes.push('border-round-bottom-3xl', 'border-round-top-lg')
+    } else if (this.isFirstInSequence && this.isLastInSequence) {
+      classes.push('border-round-bottom-3xl', 'border-round-top-3xl')
+    } else {
+      classes.push('border-round-bottom-lg', 'border-round-top-lg')
+    }
+
+    return classes
+  }
+
+  get dynamicClasses() {
+    return [...this.radiusClasses, ...this.senderClasses]
   }
 }
