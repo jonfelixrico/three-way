872f7ddc4dcdddae5da6903942f1085a51ea4a26
Felix <jonfelixrico@gmail.com>
2024-05-05 16:56:37 +0800

    Change QS parser

--- backend-nest/src/websocket/websocket/websocket.gateway.ts
+++ backend-nest/src/websocket/websocket/websocket.gateway.ts
@@ -1,9 +1,18 @@
 import { OnGatewayConnection, WebSocketGateway } from '@nestjs/websockets'
 import { Socket } from 'socket.io'
+import { parse } from 'qs'
+
+function getQuery(request: Socket['request']) {
+  const idx = request.url!.indexOf('?')
+  return parse(request.url!.slice(idx), {
+    ignoreQueryPrefix: true,
+  })
+}
 
 @WebSocketGateway()
 export class WebsocketGateway implements OnGatewayConnection {
   handleConnection(socket: Socket) {
-    console.log(socket.id)
+    const { userId } = getQuery(socket.request)
+    console.log(userId)
   }
 }
