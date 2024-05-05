938697315e8329ae5079b94d201445d603b24e3d
Felix <jonfelixrico@gmail.com>
2024-05-05 17:00:41 +0800

    Revert "Create module for event bus"
    This reverts commit 596445e64b5949a301f733efc12b87a6c037ef8a.

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common'
 import { DatasourceModule } from './datasource/datasource.module'
 import { ChatRoomModule } from './chat-room/chat-room.module'
 import { WebsocketModule } from './websocket/websocket.module'
-import { EventBusModule } from './event-bus/event-bus.module'
 
 @Module({
-  imports: [DatasourceModule, ChatRoomModule, WebsocketModule, EventBusModule],
+  imports: [DatasourceModule, ChatRoomModule, WebsocketModule],
   controllers: [],
   providers: [],
 })
