dd9302b2f1f757d2664cec36eb5a8125ec841d6a
Felix <jonfelixrico@gmail.com>
2024-05-05 15:34:39 +0800

    Create module for WS

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DatasourceModule } from './datasource/datasource.module'
 import { ChatRoomModule } from './chat-room/chat-room.module'
+import { WebsocketModule } from './websocket/websocket.module'
 
 @Module({
-  imports: [DatasourceModule, ChatRoomModule],
+  imports: [DatasourceModule, ChatRoomModule, WebsocketModule],
   controllers: [],
   providers: [],
 })
