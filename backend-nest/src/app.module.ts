596445e64b5949a301f733efc12b87a6c037ef8a
Felix <jonfelixrico@gmail.com>
2024-05-05 16:59:58 +0800

    Create module for event bus

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { DatasourceModule } from './datasource/datasource.module'
 import { ChatRoomModule } from './chat-room/chat-room.module'
 import { WebsocketModule } from './websocket/websocket.module'
+import { EventBusModule } from './event-bus/event-bus.module'
 
 @Module({
-  imports: [DatasourceModule, ChatRoomModule, WebsocketModule],
+  imports: [DatasourceModule, ChatRoomModule, WebsocketModule, EventBusModule],
   controllers: [],
   providers: [],
 })
