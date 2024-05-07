d6078720986a1d3dd38ae15e21d7dc0b27d79a2c
Felix <jonfelixrico@gmail.com>
2024-05-07 23:18:12 +0800

    Use the public decorator for the authenticate route

--- backend-nest/src/auth/auth.controller/auth.controller.ts
+++ backend-nest/src/auth/auth.controller/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { LocalAuthGuard } from 'src/auth/local-auth.guard'
+import { Public } from 'src/auth/public.decorator'
 import { IUser } from 'src/user/user.types'
 
 @Controller('auth')
@@ -8,6 +9,7 @@ export class AuthController {
   constructor(private jwt: JwtService) {}
 
   @UseGuards(LocalAuthGuard)
+  @Public()
   @Post()
   async authenticate(@Request() req: { user: IUser }) {
     const { id } = req.user
