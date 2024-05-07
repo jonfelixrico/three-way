c353f55011b3a037ff65753b1b099be43500d452
Felix <jonfelixrico@gmail.com>
2024-05-07 20:14:15 +0800

    Create DB for users

--- backend-nest/src/user/user.module.ts
+++ backend-nest/src/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user/user.service'
 import { UserAuthService } from './user-auth/user-auth.service'
+import { provideUserDb } from 'src/user/user-db.providers'
 
 @Module({
-  providers: [UserService, UserAuthService],
+  providers: [UserService, UserAuthService, provideUserDb()],
 })
 export class UserModule {}
