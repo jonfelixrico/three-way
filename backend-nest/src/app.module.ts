b09f1c50a97905aa35011abe56e43a4f9913d2c7
Felix <jonfelixrico@gmail.com>
2024-05-07 20:06:35 +0800

    Create user module

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { DatasourceModule } from './datasource/datasource.module'
 import { ChatRoomModule } from './chat-room/chat-room.module'
 import { WebsocketModule } from './websocket/websocket.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [DatasourceModule, ChatRoomModule, WebsocketModule],
+  imports: [DatasourceModule, ChatRoomModule, WebsocketModule, UserModule],
   controllers: [],
   providers: [],
 })
