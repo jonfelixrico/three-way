d66c69bdb3f1ec9c83dd59de13f00963314f1f34
Felix <jonfelixrico@gmail.com>
2024-05-07 21:19:11 +0800

    Integrate accessToken into the auth route

--- backend-nest/src/auth/auth.controller/auth.controller.ts
+++ backend-nest/src/auth/auth.controller/auth.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { AuthGuard } from '@nestjs/passport'
 import { IUser } from 'src/user/user.types'
 
 @Controller('auth')
 export class AuthController {
+  constructor(private jwt: JwtService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post()
   async authenticate(@Request() req: { user: IUser }) {
     const { id } = req.user
-    return { id }
+    const token = await this.jwt.signAsync({
+      sub: id,
+    })
+
+    return {
+      accessToken: token,
+    }
   }
 }
