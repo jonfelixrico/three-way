22a0c3eb0d7074e466ebcf7effeb1972d7d74bff
Felix <jonfelixrico@gmail.com>
2024-05-07 20:29:15 +0800

    Create user controller

--- backend-nest/src/user/user.module.ts
+++ backend-nest/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user.service/user.service'
 import { provideUserDb } from 'src/user/user-db.providers'
+import { UserController } from './user.controller/user.controller'
 
 @Module({
   providers: [UserService, provideUserDb()],
+  controllers: [UserController],
 })
 export class UserModule {}
