7bbfcb5b7c7bdeeb860d3bf2799fc3e0486a632e
Felix <jonfelixrico@gmail.com>
2024-05-07 20:53:57 +0800

    Start initial draft of the functionality

--- backend-nest/src/auth/auth.controller/auth.controller.ts
+++ backend-nest/src/auth/auth.controller/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common'
+import { CredentialsReqDto } from 'src/user/user.dtos'
+import { UserService } from 'src/user/user.service/user.service'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userSvc: UserService) {}
+
+  @Post()
+  async authenticate(@Body() body: CredentialsReqDto) {
+    const matchingUser = await this.userSvc.verifyCredentials(
+      body.username,
+      body.password
+    )
+
+    if (!matchingUser) {
+      throw new HttpException('Wrong credentials', HttpStatus.UNAUTHORIZED)
+    }
+  }
+}
