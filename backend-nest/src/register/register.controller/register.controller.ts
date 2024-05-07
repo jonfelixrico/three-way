92dee249d2eca0af1b55cdd17738b744fc47596f
Felix <jonfelixrico@gmail.com>
2024-05-07 21:57:51 +0800

    Make the register route public

--- backend-nest/src/register/register.controller/register.controller.ts
+++ backend-nest/src/register/register.controller/register.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { Public } from 'src/auth/public.decorator'
 import { CredentialsReqDto } from 'src/user/user.dtos'
 import { UserService } from 'src/user/user.service/user.service'
 
@@ -6,6 +7,7 @@ import { UserService } from 'src/user/user.service/user.service'
 export class RegisterController {
   constructor(private userSvc: UserService) {}
 
+  @Public()
   @Post()
   async register(@Body() credentials: CredentialsReqDto): Promise<void> {
     await this.userSvc.create(credentials)
