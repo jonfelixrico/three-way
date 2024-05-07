0446d656901a530d24c8092d7ef48b0659ac6d17
Felix <jonfelixrico@gmail.com>
2024-05-07 21:17:02 +0800

    Import JwtModule

--- backend-nest/src/auth/auth.module.ts
+++ backend-nest/src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller/auth.controller'
 import { UserModule } from 'src/user/user.module'
 import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      // TODO take secrets form env
+      secret: 'test',
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
