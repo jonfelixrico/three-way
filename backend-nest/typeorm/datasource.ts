344a416dd183b5fb6347773e01503214ffeee290
Felix <jonfelixrico@gmail.com>
2024-05-08 20:05:51 +0800

    Create DataSource

---
+++ backend-nest/typeorm/datasource.ts
@@ -0,0 +1,12 @@
+export default {
+  type: 'sqlite',
+  database: 'db',
+  entities: [
+    /*
+     * Even though we strictly use ts, we still need to include js in the glob.
+     * If we don't, the app will not work properly when ran when already transpiled to js
+     */
+    __dirname + '/src/**/*.entity.{ts,js}',
+  ],
+  synchronize: true,
+}
