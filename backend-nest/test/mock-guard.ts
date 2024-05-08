58532d6f0a25f3a931046ff441fa53710a05390a
Felix <jonfelixrico@gmail.com>
2024-05-08 22:14:36 +0800

    Adjust guard to use the id of seed-1

--- backend-nest/test/mock-guard.ts
+++ backend-nest/test/mock-guard.ts
@@ -4,7 +4,7 @@ export class MockGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const http = context.switchToHttp()
     http.getRequest().user = {
-      id: 'test',
+      id: '20354d7a-e4fe-47af-8ff6-187bca92f3f9',
     }
 
     return true
