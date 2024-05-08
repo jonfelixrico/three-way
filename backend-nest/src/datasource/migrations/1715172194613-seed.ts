e7235640e37e380bd7eabc2063d0ce5211e049f5
Felix <jonfelixrico@gmail.com>
2024-05-08 20:51:28 +0800

    Create seed migration

---
+++ backend-nest/src/datasource/migrations/1715172194613-seed.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class Seed1715172194613 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      // password is "p@ssw0rd"
+      `INSERT INTO "user"("id", "username", "encryptedPassword") VALUES ("20354d7a-e4fe-47af-8ff6-187bca92f3f9", "seed-1", "$2y$10$hV89MATrcSZ92N86idD1re45zKz96Bqi6WFRku3jEMEuPhGo6KtjC")`
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DELETE FROM "user" WHERE id = "20354d7a-e4fe-47af-8ff6-187bca92f3f9"`
+    )
+  }
+}
