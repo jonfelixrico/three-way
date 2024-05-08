0d3ccd4fbd91fc37a22429b4b19fe5a86d1438cd
Felix <jonfelixrico@gmail.com>
2024-05-08 20:40:57 +0800

    Adjust datasource

--- backend-nest/src/datasource/datasource.provider.ts
+++ backend-nest/src/datasource/datasource.provider.ts
@@ -1,10 +1,13 @@
 import { Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { DATASOURCE_PROVIDER } from 'src/datasource/datasource.constants'
 import { DataSource } from 'typeorm'
 
 export const DatasourceProvider: Provider = {
   provide: DATASOURCE_PROVIDER,
-  useFactory: async () => {
+  useFactory: async (cfg: ConfigService) => {
+    const inProduction = cfg.get('NODE_ENV') === 'production'
+
     const dataSource = new DataSource({
       type: 'sqlite',
       database: 'db',
@@ -13,13 +16,16 @@ export const DatasourceProvider: Provider = {
          * Even though we strictly use ts, we still need to include js in the glob.
          * If we don't, the app will not work properly when ran when already transpiled to js
          */
-        __dirname + '/../**/*.entity.{ts,js}',
+        `${__dirname}/../**/*.entity.{ts,js}`,
       ],
-      // TODO remove once we're starting to work on production-ready code
-      synchronize: true,
-      migrations: [__dirname + '/migrations/*-migration.ts'],
+      synchronize: !inProduction,
+      migrations: [
+        `${__dirname}/migrations/*-migration.{ts,js}`,
+        inProduction ? null : `${__dirname}/migrations/*-seed.{ts,js}`,
+      ],
     })
 
     return dataSource.initialize()
   },
+  inject: [ConfigService],
 }
