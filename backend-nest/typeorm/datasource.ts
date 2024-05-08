31d501b5d1f409eced25c69426f55b21bb6f07c1
Felix <jonfelixrico@gmail.com>
2024-05-08 20:18:17 +0800

    Fix the transaction datasource

--- backend-nest/typeorm/datasource.ts
+++ backend-nest/typeorm/datasource.ts
@@ -8,7 +8,7 @@ export default new DataSource({
      * Even though we strictly use ts, we still need to include js in the glob.
      * If we don't, the app will not work properly when ran when already transpiled to js
      */
-    __dirname + '/src/**/*.entity.{ts,js}',
+    __dirname + '/../src/**/*.entity.{ts,js}',
   ],
   synchronize: true,
 })
