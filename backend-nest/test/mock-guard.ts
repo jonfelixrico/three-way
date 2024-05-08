5898714398b09d2a300ace3ef955ee6ec5e991ec
Felix <jonfelixrico@gmail.com>
2024-05-08 22:18:51 +0800

    Add comment to the mock guard

--- backend-nest/test/mock-guard.ts
+++ backend-nest/test/mock-guard.ts
@@ -4,6 +4,7 @@ export class MockGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const http = context.switchToHttp()
     http.getRequest().user = {
+      // id of user seed-1. see *-seed.ts under the migrations folder for the origin
       id: '20354d7a-e4fe-47af-8ff6-187bca92f3f9',
     }
 
