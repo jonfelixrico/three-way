f9b426ff8a4a86524d9dcad5e9bc369573976091
Felix <jonfelixrico@gmail.com>
2024-05-11 01:06:49 +0800

    Implement token injection

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule, inject } from '@angular/core'
 import {
   BrowserModule,
   provideClientHydration,
@@ -7,11 +7,18 @@ import {
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { provideHttpClient, withFetch } from '@angular/common/http'
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http'
 import { provideLocalStorage } from './localstorage.provider'
 import { StoreModule } from './store/store.module'
 import { RealtimeModule } from '@/realtime/realtime.module'
 import { UserModule } from '@/user/user.module'
+import { IdentityService } from '@/user/identity.service'
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +32,25 @@ import { UserModule } from '@/user/user.module'
   ],
   providers: [
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+          const identitySvc = inject(IdentityService)!
+          const token = identitySvc.getAccessToken()
+
+          if (!token) {
+            return next(req)
+          }
+
+          return next(
+            req.clone({
+              headers: req.headers.append('Authorization', `Bearer ${token}`),
+            })
+          )
+        },
+      ])
+    ),
     provideLocalStorage(),
   ],
   bootstrap: [AppComponent],
