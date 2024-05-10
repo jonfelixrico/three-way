d950881295c87aa97ff23b89fae136c4dba3cddc
Felix <jonfelixrico@gmail.com>
2024-05-10 19:40:00 +0800

    Add form modules

--- client-angular/src/app/register-page/register-page.module.ts
+++ client-angular/src/app/register-page/register-page.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common'
 
 import { RegisterPageRoutingModule } from './register-page-routing.module'
 import { RegisterPageComponent } from './register-page.component'
+import { InputTextModule } from 'primeng/inputtext'
+import { PasswordModule } from 'primeng/password'
 
 @NgModule({
   declarations: [RegisterPageComponent],
-  imports: [CommonModule, RegisterPageRoutingModule],
+  imports: [
+    CommonModule,
+    RegisterPageRoutingModule,
+    InputTextModule,
+    PasswordModule,
+  ],
 })
 export class RegisterPageModule {}
