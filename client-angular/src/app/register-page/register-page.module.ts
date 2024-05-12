b527d38cf7acca066ef616338f38f745ace3be90
Felix <jonfelixrico@gmail.com>
2024-05-12 14:04:33 +0800

    Remove unneeded components

--- client-angular/src/app/register-page/register-page.module.ts
+++ client-angular/src/app/register-page/register-page.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common'
 import { RegisterPageRoutingModule } from './register-page-routing.module'
 import { RegisterPageComponent } from './register-page.component'
 import { InputTextModule } from 'primeng/inputtext'
-import { IconFieldModule } from 'primeng/iconfield'
-import { InputIconModule } from 'primeng/inputicon'
 import { ReactiveFormsModule } from '@angular/forms'
 import { ButtonModule } from 'primeng/button'
 import { CardModule } from 'primeng/card'
@@ -16,8 +14,6 @@ import { CardModule } from 'primeng/card'
     CommonModule,
     RegisterPageRoutingModule,
     InputTextModule,
-    IconFieldModule,
-    InputIconModule,
     ReactiveFormsModule,
     ButtonModule,
     CardModule,
