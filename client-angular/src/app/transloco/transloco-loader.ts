a2f3e7a950de90e3db32685ebe9af74e804684a0
Felix <jonfelixrico@gmail.com>
2024-05-12 15:33:58 +0800

    Create environments

--- client-angular/src/app/transloco/transloco-loader.ts
+++ client-angular/src/app/transloco/transloco-loader.ts
@@ -1,14 +1,21 @@
-import { inject, Injectable } from '@angular/core'
+import { Injectable } from '@angular/core'
 import { Translation, TranslocoLoader } from '@jsverse/transloco'
 import { HttpClient } from '@angular/common/http'
+import { PlatformService } from '@/platform.service'
+import { environment } from '@/environments/environment'
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-  private http = inject(HttpClient)
+  constructor(
+    private http: HttpClient,
+    private platformSvc: PlatformService
+  ) {}
 
   getTranslation(lang: string) {
     return this.http.get<Translation>(
-      `${environment.baseUrl}/assets/i18n/${lang}.json`
+      this.platformSvc.isBrowser
+        ? `/assets/i18n/${lang}.json`
+        : `${environment.baseUrl}/assets/i18n/${lang}.json`
     )
   }
 }
