250876d9e331a3a27bc03dd430920c246163cb1c
Felix <jonfelixrico@gmail.com>
2024-05-12 17:16:02 +0800

    Add cmt

--- client-angular/src/app/common-components/text-dialog-content/text-dialog-content.component.ts
+++ client-angular/src/app/common-components/text-dialog-content/text-dialog-content.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core'
 
+/**
+ * Just renders the text.
+ *
+ * This is intended as a utility for dynamic components if all we want to do is to render some text for the content.
+ */
 @Component({
   selector: 'app-text-dialog-content',
   standalone: true,
