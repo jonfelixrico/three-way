c2b7f67ac57d5c9d5d6f2b0cfa8cbee4a99995a5
Felix <jonfelixrico@gmail.com>
2024-05-13 19:37:53 +0800

    Add modules

--- client-angular/src/app/register-page/register-page.module.ts
+++ client-angular/src/app/register-page/register-page.module.ts
@@ -1,6 +1,6 @@
 import { TranslocoModule } from '@jsverse/transloco'
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgIf } from '@angular/common'
 
 import { RegisterPageRoutingModule } from './register-page-routing.module'
 import { RegisterPageComponent } from './register-page.component'
@@ -12,6 +12,8 @@ import { InputSubtextComponent } from '@/common-components/input-help-text/input
 import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog'
 import { TextDialogContentComponent } from '@/common-components/text-dialog-content/text-dialog-content.component'
 import { UsernameAsyncValidator } from '@/register-page/username.async-validator'
+import { IconFieldModule } from 'primeng/iconfield'
+import { InputIconModule } from 'primeng/inputicon'
 
 @NgModule({
   declarations: [RegisterPageComponent],
@@ -26,6 +28,9 @@ import { UsernameAsyncValidator } from '@/register-page/username.async-validator
     InputSubtextComponent,
     DynamicDialogModule,
     TextDialogContentComponent,
+    IconFieldModule,
+    InputIconModule,
+    NgIf,
   ],
   providers: [DialogService, UsernameAsyncValidator],
 })
