37bf614d8085044a801de8e29a778b2bce88cea4
Felix <jonfelixrico@gmail.com>
2024-05-13 20:09:50 +0800

    Create layout component

---
+++ client-angular/src/app/chat-page/chat-layout/chat-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-chat-layout',
+  templateUrl: './chat-layout.component.html',
+  styleUrl: './chat-layout.component.scss',
+})
+export class ChatLayoutComponent {}
