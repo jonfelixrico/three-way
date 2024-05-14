fad17d19c248bd4f62224a7d52d818ae6b2bdc45
Felix <jonfelixrico@gmail.com>
2024-05-14 23:38:20 +0800

    Change how we do primary keys

--- backend-nest/src/chat-room/entity/chat-room-member.entity.ts
+++ backend-nest/src/chat-room/entity/chat-room-member.entity.ts
@@ -1,29 +1,28 @@
 import { IChatRoomMember } from 'src/chat-room/chat-room.types'
 import { ChatRoom } from 'src/chat-room/entity/chat-room.entity'
 import { User } from 'src/user/user.entity'
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm'
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm'
 
 @Entity()
 export class ChatRoomMember implements IChatRoomMember {
+  /*
+   * This entity is quite special since it uses a composite PK that uses FKs
+   * See https://stackoverflow.com/a/72764767 for the reference.
+   */
+
+  @PrimaryColumn()
+  chatId: string
+
+  @PrimaryColumn()
+  userId: string
+
   @ManyToOne(() => ChatRoom, {
     eager: true,
   })
-  @JoinColumn({ name: 'chatId' })
-  @PrimaryColumn({ name: 'chatId', type: 'varchar' })
   chat: ChatRoom
 
   @ManyToOne(() => User, { eager: true })
-  @JoinColumn({ name: 'userId' })
-  @PrimaryColumn({ name: 'userId', type: 'varchar' })
   user: User
-
-  get chatId() {
-    return this.chat.id
-  }
-
-  get userId() {
-    return this.user.id
-  }
 
   @Column()
   isOwner: boolean
