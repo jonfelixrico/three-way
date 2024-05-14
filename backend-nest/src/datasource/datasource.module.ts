46f6150850b41cd73d619b818f974a32ee980401
Felix <jonfelixrico@gmail.com>
2024-05-14 20:27:14 +0800

    Use TypeOrmModule

--- backend-nest/src/datasource/datasource.module.ts
+++ backend-nest/src/datasource/datasource.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common'
-import { DatasourceProvider } from 'src/datasource/datasource.provider'
+import { ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  providers: [DatasourceProvider],
-  exports: [DatasourceProvider],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+
+      useFactory: async (cfg: ConfigService) => {
+        const inProduction = cfg.get('NODE_ENV') === 'production'
+
+        return {
+          type: 'sqlite',
+          database: 'db.sqlite',
+          entities: [
+            /*
+             * Even though we strictly use ts, we still need to include js in the glob.
+             * If we don't, the app will not work properly when ran when already transpiled to js
+             */
+            `${__dirname}/../**/*.entity.{ts,js}`,
+          ],
+          migrations: [
+            `${__dirname}/migrations/*-migration.{ts,js}`,
+            inProduction ? null : `${__dirname}/migrations/*-seed.{ts,js}`,
+          ].filter(Boolean),
+          migrationsRun: true,
+
+          logging: true,
+        }
+      },
+    }),
+  ],
 })
 export class DatasourceModule {}
