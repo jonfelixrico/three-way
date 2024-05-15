a06192ed162de4765aca45bfabcd4963aecaa737
Felix <jonfelixrico@gmail.com>
2024-05-15 23:31:02 +0800

    Impl chat list

--- client-angular/src/app/chat-page/chat-list/chat-list.component.ts
+++ client-angular/src/app/chat-page/chat-list/chat-list.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core'
+import { Observable, map } from 'rxjs'
+import { ChatService } from '@/chat-services/chat.service'
+import { Component, OnInit, Signal } from '@angular/core'
+import { Select } from '@ngxs/store'
+import { ChatSliceModel } from '@/chat-services/chat.slice'
+import { Chat } from '@/chat-services/chat-rest-api.types'
+import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-chat-list',
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.scss',
 })
-export class ChatListComponent {}
+export class ChatListComponent implements OnInit {
+  @Select() chat$!: Observable<ChatSliceModel>
+
+  chats: Signal<Chat[]>
+
+  constructor(private chatSvc: ChatService) {
+    this.chats = toSignal(
+      this.chat$.pipe(map(({ chats }) => Object.values(chats))),
+      {
+        initialValue: [],
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    this.chatSvc.loadListIntoState()
+  }
+}
--- client-angular/src/app/chat-services/chat.service.ts
+++ client-angular/src/app/chat-services/chat.service.ts
@@ -1,6 +1,22 @@
+import { Chat } from '@/chat-services/chat-rest-api.types'
+import { ChatActions } from '@/chat-services/chat.actions'
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Store } from '@ngxs/store'
+import { firstValueFrom } from 'rxjs'
 
 @Injectable()
 export class ChatService {
-  constructor() {}
+  constructor(
+    private http: HttpClient,
+    private store: Store
+  ) {}
+
+  async loadListIntoState() {
+    const chats = await firstValueFrom(this.http.get<Chat[]>('/api/chat'))
+
+    for (const chat of chats) {
+      this.store.dispatch(new ChatActions.Set(chat))
+    }
+  }
 }
