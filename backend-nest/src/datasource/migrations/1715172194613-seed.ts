1c199555894d907274709ff0f2594d28bd94b451
Felix <jonfelixrico@gmail.com>
2024-05-15 21:41:42 +0800

    Adjust password migration

--- backend-nest/src/datasource/migrations/1715172194613-seed.ts
+++ backend-nest/src/datasource/migrations/1715172194613-seed.ts
@@ -6,7 +6,7 @@ export class Seed1715172194613 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       // password is "p@ssw0rd"
-      `INSERT INTO "user"("id", "username", "encryptedPassword") VALUES ("20354d7a-e4fe-47af-8ff6-187bca92f3f9", "seed-1", "$2y$10$hV89MATrcSZ92N86idD1re45zKz96Bqi6WFRku3jEMEuPhGo6KtjC")`
+      `INSERT INTO "user"("id", "username", "encryptedPassword") VALUES ("20354d7a-e4fe-47af-8ff6-187bca92f3f9", "seed-1", "$2a$10$PXpjUXwTKUBcVbbUPxEjvuviHPGIm2wzMJum/Nxd9O8kJbIFd69sW")`
     )
   }
 
