f4468b9504bffe05a25891d65afcb1e9b6376b46
Felix <jonfelixrico@gmail.com>
2024-05-16 20:11:57 +0800

    Remove the use of toSignal

--- client-angular/src/app/chat-page/chat-list/chat-list.component.ts
+++ client-angular/src/app/chat-page/chat-list/chat-list.component.ts
@@ -1,10 +1,8 @@
 import { Observable, map } from 'rxjs'
 import { ChatService } from '@/chat-services/chat.service'
-import { Component, Signal, afterNextRender } from '@angular/core'
+import { Component, afterNextRender } from '@angular/core'
 import { Select } from '@ngxs/store'
 import { ChatSliceModel } from '@/chat-services/chat.slice'
-import { Chat } from '@/chat-services/chat-rest-api.types'
-import { toSignal } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-chat-list',
@@ -14,16 +12,11 @@ import { toSignal } from '@angular/core/rxjs-interop'
 export class ChatListComponent {
   @Select() chat$!: Observable<ChatSliceModel>
 
-  chats: Signal<Chat[]>
+  get chatList$() {
+    return this.chat$.pipe(map(({ chats }) => Object.values(chats)))
+  }
 
   constructor(chatSvc: ChatService) {
-    this.chats = toSignal(
-      this.chat$.pipe(map(({ chats }) => Object.values(chats))),
-      {
-        initialValue: [],
-      }
-    )
-
     afterNextRender(() => {
       chatSvc.loadListIntoState()
     })
