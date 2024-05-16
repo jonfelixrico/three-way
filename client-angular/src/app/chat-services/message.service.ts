413c0347609431185d3f30aa4d2b7ab82dec0afd
Felix <jonfelixrico@gmail.com>
2024-05-16 22:42:04 +0800

    Set up listener

--- client-angular/src/app/chat-services/message.service.ts
+++ client-angular/src/app/chat-services/message.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@angular/core'
 import { ChatMessage } from './chat-rest-api.types'
 import { HttpClient } from '@angular/common/http'
-import { firstValueFrom } from 'rxjs'
+import { filter, firstValueFrom, map } from 'rxjs'
+import { RealtimeService } from '@/realtime/realtime.service'
+import { Store } from '@ngxs/store'
+import { ChatActions } from '@/chat-services/chat.actions'
 
 @Injectable()
 export class MessageService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    realtimeSvc: RealtimeService,
+    store: Store
+  ) {
+    realtimeSvc
+      .getEvents$<{
+        MESSAGE_SENT: ChatMessage
+      }>()
+      .pipe(
+        map(({ MESSAGE_SENT }) => MESSAGE_SENT),
+        filter(Boolean)
+      )
+      .subscribe((payload) => {
+        store.dispatch(
+          new ChatActions.AddMessages(payload.chatRoomId, [payload])
+        )
+      })
+  }
 
   async getMessages(chatId: string) {
     return await firstValueFrom(
