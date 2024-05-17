3ba6c2fe8aa6eba9909758223e6b9ac7fcf0a950
Felix <jonfelixrico@gmail.com>
2024-05-18 00:51:26 +0800

    Add functionality

--- client-angular/src/app/chat-route-group/service.create-chat/create-chat.service.ts
+++ client-angular/src/app/chat-route-group/service.create-chat/create-chat.service.ts
@@ -1,6 +1,23 @@
+import { CreateDialogContentComponent } from '@/chat-route-group/create-dialog-content/create-dialog-content.component'
+import { ChatService } from '@/chat-services/chat.service'
 import { Injectable } from '@angular/core'
+import { DialogService } from 'primeng/dynamicdialog'
 
 @Injectable()
 export class CreateChatService {
-  constructor() {}
+  constructor(
+    private dialogSvc: DialogService,
+    private chatSvc: ChatService
+  ) {}
+
+  openCreateDialog() {
+    const ref = this.dialogSvc.open(CreateDialogContentComponent, {
+      // TODO i18nize
+      header: 'Create Dialog',
+    })
+
+    ref.onClose.subscribe((chat: { name: string }) => {
+      // TODO do something about the chat
+    })
+  }
 }
