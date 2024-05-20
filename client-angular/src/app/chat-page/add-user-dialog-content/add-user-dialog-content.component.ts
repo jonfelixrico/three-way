40a26fe6a5855a8d49661f5b04c2747dc240df4b
Felix <jonfelixrico@gmail.com>
2024-05-20 20:14:57 +0800

    Add content

--- client-angular/src/app/chat-page/add-user-dialog-content/add-user-dialog-content.component.ts
+++ client-angular/src/app/chat-page/add-user-dialog-content/add-user-dialog-content.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { DynamicDialogRef } from 'primeng/dynamicdialog'
 
 @Component({
   selector: 'app-add-user-dialog-content',
   templateUrl: './add-user-dialog-content.component.html',
   styleUrl: './add-user-dialog-content.component.scss',
 })
-export class AddUserDialogContentComponent {}
+export class AddUserDialogContentComponent {
+  form = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  })
+
+  constructor(private ref: DynamicDialogRef) {}
+
+  submit() {
+    if (!this.form.valid) {
+      return
+    }
+
+    this.ref.close(this.form.value)
+  }
+}
