b3d1e366a633375d8eab46950663b46bd1d2c37a
Felix <jonfelixrico@gmail.com>
2024-05-20 20:39:04 +0800

    Create method for opening dialog service

--- client-angular/src/app/chat-page/service.add-user/add-user.service.ts
+++ client-angular/src/app/chat-page/service.add-user/add-user.service.ts
@@ -1,6 +1,25 @@
+import { AddUserDialogContentComponent } from '@/chat-page/add-user-dialog-content/add-user-dialog-content.component'
+import { ChatService } from '@/chat-services/chat.service'
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { DialogService } from 'primeng/dynamicdialog'
 
 @Injectable()
 export class AddUserService {
-  constructor() {}
+  constructor(
+    private dialogSvc: DialogService,
+    private chatSvc: ChatService,
+    private http: HttpClient
+  ) {}
+
+  openDialog(chatId: string) {
+    const ref = this.dialogSvc.open(AddUserDialogContentComponent, {
+      // TODO i18nize this
+      header: 'Add User',
+    })
+
+    ref.onClose.subscribe(async (data: { userIds: string[] }) => {
+      this.chatSvc.addUserToChat(chatId, data)
+    })
+  }
 }
