8101eb925ea53867e3b7aa6979439cb30440210e
Felix <jonfelixrico@gmail.com>
2024-05-20 21:46:44 +0800

    Impl the search functionality

--- client-angular/src/app/chat-page/add-user-dialog-content/add-user-dialog-content.component.ts
+++ client-angular/src/app/chat-page/add-user-dialog-content/add-user-dialog-content.component.ts
@@ -1,20 +1,39 @@
+import { UserService } from '@/user/user.service'
 import { User } from '@/user/user.types'
-import { Component, Signal, computed, signal } from '@angular/core'
+import { Component, OnDestroy, Signal, computed, signal } from '@angular/core'
 import { DynamicDialogRef } from 'primeng/dynamicdialog'
+import { BehaviorSubject, Subscription, debounceTime, switchMap } from 'rxjs'
 
 @Component({
   selector: 'app-add-user-dialog-content',
   templateUrl: './add-user-dialog-content.component.html',
   styleUrl: './add-user-dialog-content.component.scss',
 })
-export class AddUserDialogContentComponent {
-  searchTerm: string = ''
+export class AddUserDialogContentComponent implements OnDestroy {
+  searchTerm$ = new BehaviorSubject('')
   searchResults: User[] = []
+  private searchSub: Subscription
 
   toAdd: Signal<User[]> = signal([])
   alreadyAdded = computed(() => new Set(this.toAdd().map(({ id }) => id)))
 
-  constructor(private ref: DynamicDialogRef) {}
+  constructor(
+    private ref: DynamicDialogRef,
+    userSvc: UserService
+  ) {
+    this.searchSub = this.searchTerm$
+      .pipe(
+        debounceTime(500),
+        switchMap((username) => userSvc.listByUsername$(username))
+      )
+      .subscribe((users: User[]) => {
+        this.searchResults = users
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.searchSub.unsubscribe()
+  }
 
   submit() {
     this.ref.close({ userIds: this.toAdd().map(({ id }) => id) })
--- client-angular/src/app/user/user.service.ts
+++ client-angular/src/app/user/user.service.ts
@@ -1,19 +1,16 @@
 import { User } from '@/user/user.types'
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { firstValueFrom } from 'rxjs'
 
 @Injectable()
 export class UserService {
   constructor(private http: HttpClient) {}
 
-  async findByUsername(username: string): Promise<User[]> {
-    return await firstValueFrom(
-      this.http.get<User[]>('/api/user', {
-        params: {
-          username,
-        },
-      })
-    )
+  listByUsername$(username: string) {
+    return this.http.get<User[]>('/api/user', {
+      params: {
+        username,
+      },
+    })
   }
 }
