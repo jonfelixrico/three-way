da475ead093880431ddc711497bf8c8dad278b14
Felix <jonfelixrico@gmail.com>
2024-05-21 23:02:42 +0800

    Integrate hydration

--- client-angular/src/app/chat-page/chat-page-routing.module.ts
+++ client-angular/src/app/chat-page/chat-page-routing.module.ts
@@ -1,11 +1,37 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { NgModule, inject } from '@angular/core'
+import {
+  ActivatedRouteSnapshot,
+  GuardResult,
+  RouterModule,
+  Routes,
+} from '@angular/router'
 import { ChatPageComponent } from './chat-page.component'
+import { Store } from '@ngxs/store'
+import { ChatSliceModel } from '@/chat-services/chat.slice'
+import { ChatService } from '@/chat-services/chat.service'
 
 const routes: Routes = [
   {
     path: '',
     component: ChatPageComponent,
+    canActivate: [
+      async (route: ActivatedRouteSnapshot): Promise<GuardResult> => {
+        const store = inject(Store)
+        const chatSvc = inject(ChatService)
+
+        const chatId = route.paramMap.get('chatId')!
+        const chatState = store.selectSnapshot(
+          ({ chat }: { chat: ChatSliceModel }) => chat.chats[chatId]
+        )
+
+        if (chatState?.status === 'HYDRATED') {
+          return true
+        }
+
+        await chatSvc.loadChatIntoState(chatId)
+        return true
+      },
+    ],
   },
 ]
 
