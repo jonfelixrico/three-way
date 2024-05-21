13f9e3b5776fc44b694e9249be45e6deeab60926
Felix <jonfelixrico@gmail.com>
2024-05-22 00:29:00 +0800

    Redirect if nonexistent

--- client-angular/src/app/chat-page/chat-page-routing.module.ts
+++ client-angular/src/app/chat-page/chat-page-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, inject } from '@angular/core'
 import {
   ActivatedRouteSnapshot,
   GuardResult,
+  Router,
   RouterModule,
   Routes,
 } from '@angular/router'
@@ -17,13 +18,14 @@ const routes: Routes = [
       async (route: ActivatedRouteSnapshot): Promise<GuardResult> => {
         const chatSvc = inject(ChatService)
         const messageSvc = inject(MessageService)
+        const router = inject(Router)
 
         const chatId = route.paramMap.get('chatId')!
 
         const foundChat = await chatSvc.loadChatIntoState(chatId)
         if (!foundChat) {
           console.log('Chat %s does not exist', chatId)
-          return false
+          return router.navigateByUrl('/app/chat')
         }
 
         await messageSvc.loadMessagesToState(chatId)
