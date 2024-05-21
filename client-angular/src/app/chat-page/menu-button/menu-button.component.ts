09700a0f1f0a02ab2f1b8d495e6f6143efff092e
Felix <jonfelixrico@gmail.com>
2024-05-21 21:10:53 +0800

    Use the transloco service

--- client-angular/src/app/chat-page/menu-button/menu-button.component.ts
+++ client-angular/src/app/chat-page/menu-button/menu-button.component.ts
@@ -1,5 +1,6 @@
 import { AddUserService } from '@/chat-page/service.add-user/add-user.service'
 import { Component, Input } from '@angular/core'
+import { TranslocoService } from '@jsverse/transloco'
 import { MenuItem } from 'primeng/api'
 
 @Component({
@@ -12,12 +13,14 @@ export class MenuButtonComponent {
 
   items: MenuItem[] = [
     {
-      // TODO add i18n
-      label: 'Add User',
+      label: this.t.translate('chat.menu.addUser'),
       icon: 'pi pi-user-plus',
       command: () => this.addUserSvc.openDialog(this.chatId),
     },
   ]
 
-  constructor(private addUserSvc: AddUserService) {}
+  constructor(
+    private addUserSvc: AddUserService,
+    private t: TranslocoService
+  ) {}
 }
