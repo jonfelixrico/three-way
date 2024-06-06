1b0ed7922e0aab40b95b74cf9c78161ec19479f8
Felix <jonfelixrico@gmail.com>
2024-06-06 22:23:39 +0800

    Adjust types for the chat room DTO

--- backend-nest/src/chat-room/chat-room.controller/chat-room.dtos.ts
+++ backend-nest/src/chat-room/chat-room.controller/chat-room.dtos.ts
@@ -1,14 +1,25 @@
-import { IChatRoom } from 'src/chat-room/chat-room.types'
+import { IChatRoom, IChatRoomMessage } from 'src/chat-room/chat-room.types'
 import { IUser } from 'src/user/user.types'
+
+class ChatRoomMessagePreviewDto {
+  message: IChatRoomMessage
+  sender: {
+    name: string
+  }
+}
 
 export class ChatRoomDto implements IChatRoom {
   id: string
   name: string
 
   members: IUser[]
+
+  previewMessage: ChatRoomMessagePreviewDto | null
 }
 
 export class PartialChatRoomDto implements IChatRoom {
   id: string
   name: string
+
+  previewMessage: ChatRoomMessagePreviewDto | null
 }
