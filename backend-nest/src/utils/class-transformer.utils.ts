021861118f5ed10f954ee104779b1ea35042a293
Felix <jonfelixrico@gmail.com>
2024-06-12 19:00:32 +0800

    Add comment

--- backend-nest/src/utils/class-transformer.utils.ts
+++ backend-nest/src/utils/class-transformer.utils.ts
@@ -1,5 +1,14 @@
 import { plainToInstance } from 'class-transformer'
 
+/**
+ * A wrapper around `plainToInstance`. The difference is that the second argument (plain data)
+ * in this method uses the type of the first arugment (class).
+ *
+ * @param clazz
+ * @param plain
+ * @param options
+ * @returns
+ */
 export function toInstance<T>(
   clazz: Parameters<typeof plainToInstance<T, T>>[0],
   plain: T,
