d4e3e35aba2fb69da9beacaf8f95d3793d2a266b
Felix <jonfelixrico@gmail.com>
2024-06-12 20:00:07 +0800

    Adjust the way to export

--- backend-nest/src/datasource/migrations/1715172194613-seed.ts
+++ backend-nest/src/datasource/migrations/1715172194613-seed.ts
@@ -1,18 +1,20 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export const SEED_1_ID = '20354d7a-e4fe-47af-8ff6-187bca92f3f9'
-
 export class Seed1715172194613 implements MigrationInterface {
   name = 'Seed1715172194613'
+
+  static SEED_1_ID = '20354d7a-e4fe-47af-8ff6-187bca92f3f9'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       // password is "p@ssw0rd"
-      `INSERT INTO "user"("id", "username", "encryptedPassword") VALUES ("${SEED_1_ID}", "seed-1", "$2a$10$PXpjUXwTKUBcVbbUPxEjvuviHPGIm2wzMJum/Nxd9O8kJbIFd69sW")`
+      `INSERT INTO "user"("id", "username", "encryptedPassword") VALUES ("${Seed1715172194613.SEED_1_ID}", "seed-1", "$2a$10$PXpjUXwTKUBcVbbUPxEjvuviHPGIm2wzMJum/Nxd9O8kJbIFd69sW")`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DELETE FROM "user" WHERE id = "${SEED_1_ID}"`)
+    await queryRunner.query(
+      `DELETE FROM "user" WHERE id = "${Seed1715172194613.SEED_1_ID}"`
+    )
   }
 }
