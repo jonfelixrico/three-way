7e2350b1ac5da6f2d964b2f077a243020614fb3c
Felix <jonfelixrico@gmail.com>
2024-06-18 20:09:04 +0800

    c5: test: add tests for already added users (#32)

--- backend-nest/test/chat.e2e-spec.ts
+++ backend-nest/test/chat.e2e-spec.ts
@@ -157,4 +157,19 @@ describe('chat', () => {
       )
     )
   })
+
+  test('Add user -- alread added', async () => {
+    const CHAT_ID = Seed1718191393819.SEED_ROOM_IDS[0]
+
+    await request(app.getHttpServer())
+      .post(`/chat/${CHAT_ID}/user`)
+      .send({
+        /*
+         * This is seed-2
+         * seed-1 to seed-10 are already added to the room specified above
+         */
+        userIds: [Seed1715702049970.SEED_USER_IDS[0]],
+      })
+      .expect(HttpStatus.INTERNAL_SERVER_ERROR)
+  })
 })
